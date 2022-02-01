91cee7bfe0088ee4b2852ceb8cab2cec55f0890f
Paul Bond <git@paulsbond.co.uk>
2022-02-01 15:27:19 +0000

    AF prep

--- program/testsetaf.py
+++ program/testsetaf.py
@@ -1,81 +1,16 @@
 #!/usr/bin/python3
 
 import glob
-import json
 import multiprocessing
 import os
+import tarfile
 import gemmi
 import modelcraft as mc
 import solrq
 import pdbe
-import tarfile
+import sfdata
+import testset
 import tinterweb
-
-
-def _make_search_structures(block, aligned):
-    structure = gemmi.make_structure_from_block(block)
-    assert len(structure) == 1
-    model = structure[0]
-    assert len(model) == 1
-    chain = model[0]
-    assert len(chain) == len(aligned)
-    for i in reversed(range(len(chain))):
-        if not aligned[i]:
-            del chain[i]
-    structures = {0: structure}
-    table = block.find("_ma_qa_metric_local.", ["label_seq_id", "metric_value"])
-    plddts = {int(row[0]): float(row[1]) for row in table}
-    for cutoff in 50, 70, 90:
-        structure = structure.clone()
-        chain = structure[0][0]
-        removed_any = False
-        for i in reversed(range(len(chain))):
-            plddt = plddts[chain[i].label_seq]
-            if plddt < cutoff:
-                del chain[i]
-                removed_any = True
-        if len(chain) == 0:
-            break
-        if removed_any:
-            structures[cutoff] = structure
-    return structures
-
-
-def _get_sf_data(pdb_id):
-    filename = f"r{pdb_id}sf.ent"
-    url = f"https://www.ebi.ac.uk/pdbe/entry-files/download/{filename}"
-    path = f"downloads/{filename}"
-    _download_file(url, path)
-    doc = gemmi.cif.read(path)
-    rblocks = gemmi.as_refln_blocks(doc)
-    cif2mtz = gemmi.CifToMtz()
-    mtz = cif2mtz.convert_block_to_mtz(rblocks[0])
-    ianom = next(DataItem.search(mtz, "KMKM"), None)
-    imean = next(DataItem.search(mtz, "JQ"), None)
-    fanom = next(DataItem.search(mtz, "GLGL"), None)
-    fmean = next(DataItem.search(mtz, "FQ"), None)
-    freer = FreeRFlag(ianom or imean or fanom or fmean).run().freer
-    observations = ianom or imean or fanom or fmean
-    return observations, freer
-
-
-def _run_molrep(observations, structure, copies):
-    molrep = Molrep(
-        observations=observations,
-        structure=structure,
-        number_of_monomers=copies,
-    ).run()
-    return molrep
-
-
-def _mean_plddt(structure):
-    chain = structure[0][0]
-    plddts = [atom.b_iso for residue in chain for atom in residue]
-    return sum(plddts) / len(plddts)
-
-
-
-# New
 
 
 def _alphafold_mmcif_paths():
@@ -108,7 +43,15 @@ def _get_potential_pdbs(uniprot):
     return pdb_ids, copies
 
 
-def _trim_alphafold(alphafold, residues):
+def _any_pdb_ids_already_in_set(pdb_ids):
+    for pdb_id in pdb_ids:
+        directory = os.path.join("data", "af", pdb_id)
+        if os.path.exists(directory):
+            return True
+    return False
+
+
+def _truncate_alphafold(alphafold, residues):
     structure = alphafold.clone()
     chain = structure[0][0]
     observed = [False] * len(chain)
@@ -126,9 +69,9 @@ def _trim_alphafold(alphafold, residues):
     return structure
 
 
-def _superposed_completeness(trimmed, deposited, label_asym_id):
+def _superposed_completeness(truncated, deposited, label_asym_id):
     ref_polymer = deposited[0].get_subchain(label_asym_id)
-    wrk_polymer = trimmed[0].get_subchain("A")
+    wrk_polymer = truncated[0].get_subchain("A")
     sup = gemmi.calculate_superposition(
         ref_polymer, wrk_polymer, gemmi.PolymerType.PeptideL, gemmi.SupSelect.CaP
     )
@@ -153,93 +96,93 @@ def _superposed_completeness(trimmed, deposited, label_asym_id):
 
 def _choose_pdb(pdb_ids, uniprot, alphafold):
     uniprot_data = pdbe.uniprot_data(uniprot)
+    chosen_pdb_id = None
+    chosen_completeness = 0.9
+    chosen_truncated = None
+    chosen_deposited = None
     for pdb_id in pdb_ids & uniprot_data.keys():
         entry_data = uniprot_data[pdb_id]
-        trimmed = _trim_alphafold(alphafold, entry_data["residues"])
-        if trimmed is None:
+        truncated = _truncate_alphafold(alphafold, entry_data["residues"])
+        if truncated is None:
             continue
         deposited = pdbe.structure(pdb_id)
         best_chain = entry_data["bestChainId"]
-        completeness = _superposed_completeness(trimmed, deposited, best_chain)
-        pdb_id = entry["accession"]
-        if pdb_id in pdbs:
-            mutated = False
-            aligned = [False] * length
-            for residues in entry["residues"]:
-                if residues["indexType"] == "UNIPROT":
-                    if "mutation" in residues:
-                        mutated = True
-                        break
-                    start = residues["startIndex"] - 1
-                    stop = residues["endIndex"]
-                    for index in range(start, stop):
-                        aligned[index] = True
-            aligned = aligned[begin - 1 : end]
-            if mutated or not any(aligned):
-                del pdbs[pdb_id]
-            else:
-                pdbs[pdb_id]["aligned"] = aligned
-    for key in list(pdbs.keys()):
-        if "aligned" not in pdbs[key]:
-            del pdbs[key]
-    if len(pdbs) == 0:
-        return None
-    return max(pdbs.values(), key=lambda pdb: pdb["aligned"].count(True))
+        completeness = _superposed_completeness(truncated, deposited, best_chain)
+        if 0.2 < completeness < chosen_completeness:
+            chosen_pdb_id = pdb_id
+            chosen_completeness = completeness
+            chosen_truncated = truncated
+            chosen_deposited = deposited
+    return chosen_pdb_id, chosen_truncated, chosen_deposited
+
+
+def _make_search_structures(structure):
+    structures = [structure]
+    for cutoff in 50, 70, 90:
+        structure = structure.clone()
+        chain = structure[0][0]
+        removed_any = False
+        for i in reversed(range(len(chain))):
+            plddt = chain[i][0].b_iso
+            if plddt < cutoff:
+                del chain[i]
+                removed_any = True
+        if len(chain) == 0:
+            break
+        if removed_any:
+            structures.append(structure)
+    return structures
+
+
+def _molecular_replacement(fmean, truncated, copies):
+    best_mr_score = 0
+    best_structure = None
+    search_structures = _make_search_structures(truncated)
+    for structure in search_structures:
+        molrep = mc.Molrep(fmean, structure, copies).run()
+        if molrep.n_solution == copies and molrep.mr_score > best_mr_score:
+            best_mr_score = molrep.mr_score
+            best_structure = molrep.structure
+    return best_structure
 
 
 def _prepare_case(path):
     doc = gemmi.cif.read(path)
     block = doc.sole_block()
     uniprot = block.find_value("_ma_target_ref_db_details.db_accession")
-    begin = int(block.find_value("_ma_target_ref_db_details.seq_db_align_begin"))
     end = int(block.find_value("_ma_target_ref_db_details.seq_db_align_end"))
-    if begin != 1 or end == 1400:
-        return
-    try:
-        alphafold = mc.read_structure(path)
-    except (RuntimeError, ValueError):
-        return
+    if end == 1400:
+        return "File contains residues 1-1400 of a (probably) longer sequence"
+    alphafold = mc.read_structure(path)
     pdb_ids, copies = _get_potential_pdbs(uniprot)
-    if len(pdb_ids) == 0:
+    if _any_pdb_ids_already_in_set(pdb_ids):
         return
-    pdb_id = _choose_pdb(pdb_ids, uniprot, alphafold)
-    # pdb_id = pdb["pdb_id"]
-    # length = pdb["polymer_length"]
-    # copies = pdb["number_of_copies"]
-    # observations, freer = _get_sf_data(pdb_id)
-    # metadata = {
-    #     "uniprot": uniprot,
-    #     "uniprot_begin": begin,
-    #     "uniprot_end": end,
-    #     "pdb_id": pdb_id,
-    #     "pdb_length": length,
-    #     "pdb_copies": copies,
-    #     "pdb_resolution": observations.resolution_high(),
-    # }
-    # search_structures = _make_search_structures(block, pdb["aligned"])
-    # best_mr_score = 0
-    # best_structure = None
-    # for cutoff, structure in search_structures.items():
-    #     residues = len(structure[0][0])
-    #     molrep = _run_molrep(observations, structure, copies)
-    #     if molrep.n_solution == copies and molrep.mr_score > best_mr_score:
-    #         best_mr_score = molrep.mr_score
-    #         metadata["model_plddt_cutoff"] = cutoff
-    #         metadata["model_plddt_mean"] = _mean_plddt(structure)
-    #         metadata["model_residues"] = residues
-    #         metadata["model_mr_score"] = molrep.mr_score
-    #         metadata["model_mr_zscore"] = molrep.mr_zscore
-    #         best_structure = molrep.structure
-    # if best_mr_score == 0:
-    #     return
-    # contents = _entry_contents(pdb_id)
-    # directory = f"data/{af_base}-{pdb_id}"
-    # os.makedirs(directory, exist_ok=True)
-    # contents.write_json_file(f"{directory}/contents.json")
-    # write_mtz(f"{directory}/data.mtz", [observations, freer])
-    # write_mmcif(f"{directory}/model.cif", best_structure)
-    # with open(f"{directory}/metadata.json", "w") as stream:
-    #     json.dump(metadata, stream, indent=4)
+    if len(pdb_ids) == 0:
+        return "No potential PDB entries found"
+    pdb_id, truncated, deposited = _choose_pdb(pdb_ids, uniprot, alphafold)
+    if pdb_id is None:
+        return "No PDB entries with superposed completeness between 20% and 90%"
+    rblocks = pdbe.rblocks(pdb_id)
+    fmean, freer = sfdata.fmean_rfree(rblocks[0])
+    if not sfdata.compatible_cell(deposited, [fmean, freer]):
+        return "Different cell or space group in the structure and data"
+    mc.update_cell(deposited, new_cell=fmean.cell)
+    refmac = mc.RefmacXray(deposited, fmean, freer, cycles=10).run()
+    if refmac.rfree > 0.06 * refmac.resolution_high + 0.17:
+        return "R-free for deposited structure deemed too high"
+    if refmac.data_completeness < 0.9:
+        return "Data completeness less than 90%"
+    mr_structure = _molecular_replacement(fmean, truncated, copies[pdb_id])
+    if mr_structure is None:
+        return "None of the MR models found all copies"
+    molrep_refmac = mc.RefmacXray(mr_structure, fmean, freer, cycles=10).run()
+    phasematch = mc.PhaseMatch(fmean, refmac.abcd, molrep_refmac.abcd).run()
+    if phasematch.f_map_correlation < 0.2:
+        return "F-map correlation less than 0.2"
+    directory = os.path.join("data", "af", pdb_id)
+    testset.write_case(pdb_id, directory, refmac, phasematch, fmean, freer)
+    mc.write_mmcif(f"{directory}/model.cif", mr_structure)
+
 
 def prepare():
     paths = _alphafold_mmcif_paths()
@@ -249,4 +192,5 @@ def prepare():
         "downloads/data/alphafold/AF-P59666-F1-model_v2.cif.gz",
     ]
     pool = multiprocessing.Pool()
-    pool.map(_prepare_case, paths)
+    failures = pool.map(_prepare_case, paths)
+    testset.write_failures_table("prep_failures_af.txt", failures)

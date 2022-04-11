3977f0efccc5dde620d6695d8c087d3be9a6fb15
Paul Bond <git@paulsbond.co.uk>
2022-04-11 20:54:45 +0100

    Removed request caching

--- program/tinterweb.py
+++ program/tinterweb.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import json
 import multiprocessing
 import os
 import urllib.request
@@ -8,29 +7,18 @@ import requests
 
 
 _DOWNLOADS_PATH = "downloads"
-_CACHE_PATH = "request_cache.json"
 _LOCK = multiprocessing.Lock()
 
 
 def request_json(url, data=None):
     data = data or {}
-    key = str((url, tuple(sorted(data.items()))))
     with _LOCK:
-        cache = {}
-        if os.path.exists(_CACHE_PATH):
-            with open(_CACHE_PATH) as stream:
-                cache = json.load(stream)
-        if key in cache:
-            return cache[key]
         print("Requesting:", url)
         if data:
             print("With data:", data)
             response = requests.post(url, data=data)
         else:
             response = requests.get(url)
-        cache[key] = response.json()
-        with open(_CACHE_PATH, "w") as stream:
-            json.dump(cache, stream)
         return response.json()
 
 

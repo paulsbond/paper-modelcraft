7c3badb75a7c51b4009b48631b7d1c89165b4427
Paul Bond <git@paulsbond.co.uk>
2022-07-08 11:43:46 +0100

    Ncacstat neighbor search

--- program/ncacstat.py
+++ program/ncacstat.py
@@ -2,35 +2,31 @@ import gemmi
 
 
 _NAMES = {"N", "CA", "C"}
-_TYPES = set()
-_TYPES |= {"ALA", "ARG", "ASN", "ASP", "CYS", "GLN", "GLU", "GLY", "HIS", "ILE", "LEU"}
-_TYPES |= {"LYS", "MET", "MSE", "PHE", "PRO", "SER", "THR", "TRP", "TYR", "UNK", "VAL"}
 
 
-def completeness(
-    built: gemmi.Structure, deposited: gemmi.Structure, cutoff: float = 1.0
-) -> float:
+def completeness(built: gemmi.Structure, deposited: gemmi.Structure) -> float:
     num_residues = 0
     num_residues_built = 0
-    for ref_res in _protein_residues(deposited):
-        num_residues += 1
-        for wrk_res in _protein_residues(built):
-            if all(_matching_atom(ref_res, wrk_res, name, cutoff) for name in _NAMES):
-                num_residues_built += 1
-                break
+    search = gemmi.NeighborSearch(built, max_radius=1.0).populate(include_h=False)
+    for chain in deposited[0]:
+        for residue in chain.first_conformer():
+            info = gemmi.find_tabulated_residue(residue.name)
+            if info.kind == gemmi.ResidueInfoKind.AA:
+                if all(name in residue for name in _NAMES):
+                    num_residues += 1
+                    if _residue_is_built(built, search, residue):
+                        num_residues_built += 1
     return num_residues_built / num_residues
 
 
-def _protein_residues(structure: gemmi.Structure):
-    for chain in structure[0]:
-        for residue in chain.first_conformer():
-            if residue.name in _TYPES and all(name in residue for name in _NAMES):
-                yield residue
+def _residue_is_built(built, search, residue):
+    return all(_atom_is_built(built, search, residue, name) for name in _NAMES)
 
 
-def _matching_atom(res1, res2, name, cutoff):
-    for atom1 in res1[name]:
-        for atom2 in res2[name]:
-            if atom1.pos.dist(atom2.pos) < cutoff:
+def _atom_is_built(built, search, residue, name):
+    for atom_alt in residue[name]:
+        for mark in search.find_atoms(atom_alt.pos, "\0", radius=1.0):
+            cra = mark.to_cra(built[0])
+            if cra.atom.name == name:
                 return True
     return False
